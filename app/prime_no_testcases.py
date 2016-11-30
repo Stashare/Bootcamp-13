17589ba79675366fb911e43240454f9121e8f852
Kennedy Wainaina <Kennedy Wainaina>
2016-11-30 19:44:25 +0300

    test cases

--- app/prime_no_testcases.py
+++ app/prime_no_testcases.py
@@ -9,3 +9,11 @@ class TestPrimeNumberGenerator(unittest.TestCase):
     #Tests correct output for inputs more than 50
     def test_prime_generator_large_input(self):
         self.assertEqual(prime_generator(80),[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79])
+
+        #Tests correct error message for wrong string input
+    def test_prime_generator_string_input(self):
+        self.assertEqual(prime_generator('a'),'Input must be an integer, String given')
+
+    #Tests correct error message for wrong float input
+    def test_prime_generator_float_input(self):
+        self.assertEqual(prime_generator(11.9),'Input must be an integer, Float given')
