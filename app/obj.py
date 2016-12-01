71bb85400c31b4ffa3dd372b4650b7037cb4db48
CEO <wainainaken@gmail.com>
2016-12-01 18:26:47 +0300

    Update obj.py

--- app/obj.py
+++ app/obj.py
@@ -1,4 +1,23 @@
- if len(args) >= 3:
+ class Car(object):
+    
+    def __init__(self, *args): 
+      
+      if len(args) == 0:
+        self.name = 'General'
+        self.model = 'GM'
+        
+      if len(args) >= 1:
+        self.name = args[0]
+        
+      if len(args) >= 2:
+        self.model = args[1]
+        
+      if self.name in ('Porshe', 'Koenigsegg'): 
+         self.num_of_doors = 2
+      else:
+         self.num_of_doors = 4
+         
+      if len(args) >= 3:
         self.car_type = args[2]
       else:
         self.car_type = 'saloon'
