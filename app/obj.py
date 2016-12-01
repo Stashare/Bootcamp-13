9dd99549abf57fd623e287c6cc178fc6a013455d
CEO <wainainaken@gmail.com>
2016-12-01 18:23:23 +0300

    Update obj.py

--- app/obj.py
+++ app/obj.py
@@ -1,5 +1,22 @@
-class carClass:
-		
-			
-
-honda = carClass()
+ if len(args) >= 3:
+        self.car_type = args[2]
+      else:
+        self.car_type = 'saloon'
+        
+      if self.car_type == 'trailer':
+        self.num_of_wheels = 8
+      else:
+       self.num_of_wheels = 4
+       
+      self.speed = 0
+       
+    def is_saloon(self):
+      return self.car_type == 'saloon'
+      
+    def drive(self, spd):
+      if self.car_type == 'trailer':
+        self.speed = spd * 11
+      else:
+        self.speed = 10 ** spd
+        
+      return self
