99c161448e503d350a891b5b85df338fac233be9
Kennedy Wainaina <Kennedy Wainaina>
2016-12-01 17:42:17 +0300

    Data Types

---
+++ app/datatype.py
@@ -0,0 +1,26 @@
+str = "Andela"
+print "Length of the str: ", len(str)
+
+null="None"
+
+if null is "None" :
+   print 'no value'
+
+a=1+2
+print a==6
+print a==3
+
+less=3
+
+if less < 100 :
+  print'less than 100'
+else:
+  print 'more than 100'
+
+listnum = [1,2,3]
+
+
+if len(listnum) < 3:
+	print'none'
+else:
+	print listnum[2]
